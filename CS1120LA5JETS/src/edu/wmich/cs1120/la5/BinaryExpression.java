6e34a5e953ba0e5918eff33424bcf39fbe0fc8a3
James Eckler <jameseckler1994@yahoo.com>
2017-03-22 13:26:52 -0400

    Methods for BinaryExpression and Literal classes

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -2,6 +2,10 @@ package edu.wmich.cs1120.la5;
 
 public class BinaryExpression implements IExpression {
 
+	public BinaryExpression(IExpression left, IExpression right, IOperation op) {
+		
+	}
+	
 	@Override
 	public int getValue() {
 		// TODO Auto-generated method stub
--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/Literal.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/Literal.java
@@ -2,6 +2,10 @@ package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression {
 
+	public Literal(int value) {
+		
+	}
+	
 	@Override
 	public int getValue() {
 		// TODO Auto-generated method stub
