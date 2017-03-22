4f29f0bedd8078179b2fdf37c25ba1a3b60dbcd1
James Eckler <jameseckler1994@yahoo.com>
2017-03-22 12:19:15 -0400

    Addition and Subtraction classes

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/IMapCreator.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/IMapCreator.java
@@ -1,9 +1,10 @@
 package edu.wmich.cs1120.la5;
 
+import java.io.IOException;
 
 public interface IMapCreator {
 
-	void scanTerrain(String fileName,int threshold);
+	void scanTerrain(String fileName,int threshold) throws IOException;
 	TerrainScanner getScanner();
 	void setScanner(TerrainScanner scanner);
 	
