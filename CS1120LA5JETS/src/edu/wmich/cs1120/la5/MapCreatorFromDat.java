59432cb636025aac5c5770584c511c415b469e87
James Eckler <jameseckler1994@yahoo.com>
2017-03-22 12:12:39 -0400

    MapCreatorFromDat methods

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -2,4 +2,22 @@ package edu.wmich.cs1120.la5;
 
 public class MapCreatorFromDat implements IMapCreator {
 
+	@Override
+	public void scanTerrain(String fileName, int threshold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TerrainScanner getScanner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setScanner(TerrainScanner scanner) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
