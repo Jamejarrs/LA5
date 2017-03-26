ff6de0512c0a5ab225afc57971bbececdda9f92b
Travi <Travi@10.80.226.86>
2017-03-26 18:49:14 -0400

    MapCreator from Dat finished

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,22 +1,51 @@
 package edu.wmich.cs1120.la5;
 
+import java.io.*;
+
 public class MapCreatorFromDat implements IMapCreator {
 
+	TerrainScanner scanner = new TerrainScanner();
+	
+	double currentBasicEnergy;
+	double currentElevation;
+	double currentRadiation;
+	
 	@Override
-	public void scanTerrain(String fileName, int threshold) {
-		// TODO Auto-generated method stub
+	public void scanTerrain(String fileName, int threshold) throws IOException {
+		FileInputStream fStream = new FileInputStream(fileName);
+		DataInputStream input = new DataInputStream(fStream);
+		IArea[][] terrain = new IArea[10][10];
 		
+		for (int row = 0; row < 10; row++) {
+			for (int col = 0; col < 10; col++) {
+				
+				currentBasicEnergy =input.readDouble(); 
+				currentElevation =input.readDouble(); 
+				currentRadiation =input.readDouble(); 
+				
+				if (currentRadiation >= 0.5 || currentElevation > threshold * 0.5)
+					terrain[row][col] = new HighArea();
+				else
+					terrain[row][col] = new LowArea();
+				
+				terrain[row][col].setBasicEnergyCost(currentBasicEnergy);
+				terrain[row][col].setElevation(currentElevation);
+				terrain[row][col].setRadiation(currentRadiation);
+				
+		
+			}
+		}
+		input.close();
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
-		// TODO Auto-generated method stub
-		return null;
+		return scanner;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
-		// TODO Auto-generated method stub
+		this.scanner = scanner;
 		
 	}
 
