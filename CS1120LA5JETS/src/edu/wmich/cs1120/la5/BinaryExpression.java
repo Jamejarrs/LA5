e79d342a7ae06a1e0933c1ec923ac067e23ab000
Travi <Travi@10.80.147.199>
2017-03-29 11:12:56 -0400

    Literal class and MapCreatorfromDat updated

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -4,7 +4,7 @@ public class BinaryExpression implements IExpression {
 
 	@Override
 	public int getValue() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/Literal.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/Literal.java
@@ -1,11 +1,15 @@
 package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression {
-
+	private int value;
+	
+	public Literal(int value){
+		this.value = value;
+	}
+	
 	@Override
 	public int getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return value;
 	}
 
 }
