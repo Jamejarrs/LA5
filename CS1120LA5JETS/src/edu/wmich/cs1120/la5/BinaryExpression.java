61545b8661d49beed4271004b202e7edf584e432
CS User <CS User@c208-04>
2017-03-29 11:23:02 -0400

    ExpressionFactory and JavaDoc for BinaryExpression done

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -6,14 +6,22 @@ public class BinaryExpression implements IExpression {
 	private IExpression left;
 	private IExpression right;
 	
-	
+	/**
+	 * Sets attributes of object from parameters
+	 * @param left Literal object holding left value
+	 * @param right Literal object holding right value
+	 * @param op Object holding corresponding operation based on .dat character
+	 */
 	public BinaryExpression(IExpression left, IExpression right, IOperation op) {
 		this.left = left;
 		this.right = right;
 		this.op = op;
 	}
 	
-	@Override
+	/**
+	 * Gets value of operation based on operator in .dat file
+	 * @return Value of operation using left and right values
+	 */
 	public int getValue() {
 		return op.perform(left, right);
 	}
