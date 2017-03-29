e9809a6668ad637210c6c26eb07cce1b5fbd9e2b
Travi <Travi@10.80.147.199>
2017-03-29 12:02:23 -0400

    Updated javadoc for EnergyAnalyzer, Literal and RadiationAnalyzer

--- CS1120LA5JETS/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ CS1120LA5JETS/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -6,12 +6,18 @@ public class EnergyAnalyzer implements IRover {
 	private ArrayList<IArea> path;
 	private String analysis;
 	private double energy;
-
+	/**
+	 * This method returns the path
+	 * @return path - the path for the given maze
+	 */
 	@Override
 	public ArrayList<IArea> getPath() {
 		return path;
 	}
-
+	/**
+	 * This method sets the path
+	 * @param area - The area that is set for the path
+	 */
 	@Override
 	public void setPath(ArrayList<IArea> area) {
 
@@ -19,7 +25,9 @@ public class EnergyAnalyzer implements IRover {
 
 		
 	}
-
+	/**
+	 * This method analyzes the path to calculate the energy
+	 */
 	@Override
 	public void analyzePath() {
 		energy = 0;
@@ -29,18 +37,27 @@ public class EnergyAnalyzer implements IRover {
 		setAnalysis(toString());
 
 	}
-
+	/**
+	 * This method returns the analysis
+	 * @return analysis - the given analysis that is being calculated
+	 */
 	@Override
 	public String getAnalysis() {
 		return analysis;
 	}
-
+	/**
+	 * Sets the analysis
+	 * @param analysis - the analysis based on the energy calculator
+	 */
 	@Override
 	public void setAnalysis(String analysis) {
 		this.analysis = analysis;
 
 	}
-	
+	/** 
+	 * Turns the energy to a string in order to be returned
+	 * @return energy - energy is returned as a string
+	 */
 	public String toString(){
 		return "" + energy;
 
